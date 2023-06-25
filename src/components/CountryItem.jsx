f0063f98f525f57b70430efd437245998e1bd5ca
Shubhashish77 <shubhashish941@gmail.com>
2023-06-25 20:54:38 +0530

    working...

---
+++ src/components/CountryItem.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+
+import styles from "./CountryItem.module.css";
+
+function CountryItem({ country }) {
+  console.log("country", country)
+  return (
+    <li className={styles.countryItem}>
+      <span>{country.emoji}</span>
+      <span>{country.country}</span>
+    </li>
+  );
+}
+
+export default CountryItem;
